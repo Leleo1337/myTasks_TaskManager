ae6436b8313fecfb8c14d9c96b2af41df2b4fefe
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-12 17:50:31 -0300

    xd

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,11 @@
 import express from 'express'
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express()
 const PORT = 5000
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+const __dirname = import.meta.dirname
 
 app.use(express.static(path.join(__dirname, '../frontend/public')))
-
-app.get('/', (req, res) => {
-    res.send('Hello world!') 
-})
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
