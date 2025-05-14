1a54e4ff32edac5f0e082f4f8d9917db9231e487
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-14 17:34:28 -0300

    added route not found middleware

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import path from "path";
 import tasks from "./routes/tasks.js";
+import notFound from "./middlewares/not-found.js";
 
 const app = express()
 const __dirname = import.meta.dirname;
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../frontend/public")));
+app.use(notFound)
 app.use('/api/v1/tasks', tasks)
 
 export default app
---
+++ backend/middlewares/not-found.js
@@ -0,0 +1,5 @@
+function notFound(req, res) {
+    res.status(404).send('<h1>Route does not exist! go back <a href="/">here</a></h1>')
+}
+
+export default notFound
