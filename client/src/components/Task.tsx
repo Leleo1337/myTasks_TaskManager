a5aa8266ec98d5197a76ad787d29957eddef07bb
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-15 19:22:48 -0300

    create task component

---
+++ client/src/components/Task.tsx
@@ -0,0 +1,67 @@
+import { Calendar, Edit, LucideCircle, LucideCircleCheckBig, MoreVertical, Trash2 } from "lucide-react";
+import PriorityFlag from "./PriorityFlag";
+import clsx from "clsx";
+import CustomFlag from "./CustomFlag";
+import type { taskProps } from "../types/tasks";
+
+export default function Task({ toggleCheck, checked, toggleSettings, menuOpen, toggleEditForm }: taskProps) {
+   return (
+      <>
+         <div
+            className={clsx(
+               `w-full border-l-5 ${
+                  checked ? "border-green-400" : "border-gray-300"
+               } bg-white rounded-xl drop-shadow-xs hover:drop-shadow-md  transition-all ease duration-200`
+            )}>
+            <div className="flex gap-4 p-4">
+               <div>
+                  <button onClick={toggleCheck} className="rounded-full mt-0.5 cursor-pointer">
+                     {checked ? (
+                        <LucideCircleCheckBig size={20} color="#05df72" />
+                     ) : (
+                        <LucideCircle size={20} color="#99a1af" />
+                     )}
+                  </button>
+               </div>
+               <div className="flex flex-1 flex-col">
+                  <div className="flex gap-2 mb-2 flex-wrap">
+                     <h3 className={clsx(`font-semibold ${checked ? "line-through text-gray-600" : "text-black"}`)}>
+                        exemplo teste
+                     </h3>
+                     <PriorityFlag type="Low" />
+                  </div>
+                  <div className={clsx(`text-sm pb-4 ${checked ? "text-gray-400" : "text-gray-600"}`)}>
+                     teste 123 teste 14 daslpd slppalds lpasdlpadslpasdplasd lpad lpslpdaslpdaslpasd lp
+                  </div>
+                  <div className="flex items-center gap-2 flex-wrap">
+                     <div className="flex items-center gap-1 text-red-700">
+                        <Calendar size={15} />
+                        <span className="text-xs">03/03</span>
+                     </div>
+                     <CustomFlag color="#FF0000" text="2:00" />
+                  </div>
+               </div>
+               <div className="relative">
+                  <button onClick={toggleSettings} className="hover:bg-gray-100 rounded-full p-1 cursor-pointer">
+                     <MoreVertical size={18} className="text-black" />
+                  </button>
+                  {menuOpen && (
+                     <div className="absolute right-0 top-6 w-36 bg-white p-3 space-y-3 shadow-md ring ring-gray-400/20 rounded-md">
+                        <button
+                           onClick={toggleEditForm}
+                           className="flex items-center gap-1 text-blue-500 text-sm cursor-pointer hover:text-blue-700">
+                           <Edit size={18} />
+                           <span>Edit</span>
+                        </button>
+                        <button className="flex items-center gap-1 text-red-500 text-sm hover:text-red-700 cursor-pointer">
+                           <Trash2 size={18} />
+                           <span>Delete</span>
+                        </button>
+                     </div>
+                  )}
+               </div>
+            </div>
+         </div>
+      </>
+   );
+}
