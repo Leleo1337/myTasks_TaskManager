e2d2969520cc2837729190daacb9b2615b66fe8e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-15 19:22:39 -0300

    create priority flags component

---
+++ client/src/components/PriorityFlag.tsx
@@ -0,0 +1,15 @@
+import type { priorityFlagProps } from "../types/Flags";
+import hexToRGB from "../utils/hexToRgba";
+import getFlagsColor from "../utils/priorityFlagsColor";
+
+export default function PriorityFlag({ type }: priorityFlagProps) {
+   const color = getFlagsColor(type);
+
+   return (
+      <div
+         className="flex items-center justify-center px-2 rounded-xl font-semibold text-xs"
+         style={{ backgroundColor: hexToRGB(color, "0.15"), color: hexToRGB(color, "1") }}>
+         {type}
+      </div>
+   );
+}
