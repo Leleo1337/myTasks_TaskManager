1abd394797eef07df438ef7eed9e5884d05ba2b2
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-16 19:28:03 -0300

    added cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import path from "path";
 import tasks from "./routes/tasks.js";
-import notFound from "./middlewares/not-found.js";
+import cors from 'cors'
 
 const app = express()
 const __dirname = import.meta.dirname;
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../frontend-template/public")));
 app.use('/api/v1/tasks', tasks)
