3a50d858b5369322acabd8988102249bdf054a56
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-19 17:14:48 -0300

    options menu closes when you click outside it now

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -3,6 +3,7 @@ import PriorityFlag from "./PriorityFlag";
 import clsx from "clsx";
 import CustomFlag from "./CustomFlag";
 import type { taskProps } from "../types/tasksTypes";
+import { useEffect, useRef } from "react";
 
 export default function Task({
    title,
@@ -15,17 +16,33 @@ export default function Task({
    toggleSettings,
    menuOpen,
    toggleEditForm,
-   deleteTask
+   deleteTask,
+   onCloseMenu,
 }: taskProps) {
+
+   const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+   useEffect(() => {
+      function handleClickOutsideMenu(event: Event) {
+         if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            onCloseMenu();
+         }
+      }
+
+      document.addEventListener("mousedown", handleClickOutsideMenu);
+
+      return () => document.removeEventListener("mousedown", handleClickOutsideMenu);
+   }, []);
+
    return (
       <>
          <div
             className={clsx(
                `w-full border-l-5 ${
                   completed ? "border-green-400" : "border-gray-300"
-               } bg-white rounded-xl drop-shadow-xs hover:drop-shadow-md  transition-all ease duration-200`
+               } bg-white rounded-xl shadow-sm hover:shadow-md transition-all ease duration-200`
             )}>
-            <div className="relative z-20 flex gap-4 p-4">
+            <div className="flex gap-4 p-4">
                <div>
                   <button onClick={toggleComplete} className="rounded-full mt-0.5 cursor-pointer">
                      {completed ? (
@@ -46,15 +63,13 @@ export default function Task({
                      {description}
                   </div>
                   <div className="flex items-center gap-2 flex-wrap">
-                     <div className="flex items-center gap-1 text-red-700">
-                        <Calendar size={15} />
-                        <span className="text-xs">{date}</span>
-                     </div>
-                     {tags &&  
-                        tags.map((tag, index) => (
-                           <CustomFlag key={index} text={tag.text} color={tag.color}/>
-                        ))
-                     }
+                     {date && (
+                        <div className="flex items-center gap-1 text-red-700">
+                           <Calendar size={15} />
+                           <span className="text-xs">{date}</span>
+                        </div>
+                     )}
+                     {tags && tags.map((tag, index) => <CustomFlag key={index} text={tag.text} color={tag.color} size="small"/>)}
                   </div>
                </div>
                <div className="relative">
@@ -62,14 +77,24 @@ export default function Task({
                      <MoreVertical size={18} className="text-black" />
                   </button>
                   {menuOpen && (
-                     <div className="absolute z-50 right-0 top-6 w-36 bg-white shadow-md ring ring-gray-400/20 rounded-md">
+                     <div
+                        ref={dropdownRef}
+                        className="absolute z-50 right-0 top-6 w-36 bg-white shadow-md ring ring-gray-400/20 rounded-md">
                         <button
-                           onClick={toggleEditForm}
+                           onClick={() => {
+                              toggleEditForm();
+                              onCloseMenu();
+                           }}
                            className="w-full p-3 flex items-center gap-1 text-blue-500 text-sm cursor-pointer hover:bg-gray-50">
                            <Edit size={18} />
                            <span>Edit</span>
                         </button>
-                        <button onClick={deleteTask} className="flex items-center gap-1 w-full p-3 text-red-500 text-sm hover:bg-gray-50 cursor-pointer">
+                        <button
+                           onClick={() => {
+                              deleteTask();
+                              onCloseMenu();
+                           }}
+                           className="flex items-center gap-1 w-full p-3 text-red-500 text-sm hover:bg-gray-50 cursor-pointer">
                            <Trash2 size={18} />
                            <span>Delete</span>
                         </button>
