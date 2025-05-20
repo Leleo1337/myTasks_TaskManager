0d8b43db38891cd7ebbf53dcbcb038b0d97e03bf
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-05-20 13:53:39 -0300

    added onClick & allowDelete props
    props for tasks form

--- client/src/components/CustomFlag.tsx
+++ client/src/components/CustomFlag.tsx
@@ -2,18 +2,21 @@ import clsx from "clsx";
 import type { customFlagProps } from "../types/flagsTypes";
 import hexToRGB from "../utils/hexToRgba";
 
-export default function CustomFlag({ text, color, size }: customFlagProps) {
+export default function CustomFlag({ text, color, size, allowDelete, onClick }: customFlagProps) {
    const backgroundColor = hexToRGB(color, "0.25");
    const textColor = hexToRGB(color, "1");
    return (
-      <div
-         className={clsx(
-            `px-2 py-0.5 rounded-md font-semibold`,
-            size === 'small' && `text-xs`,
-            size === 'medium' && `text-md px-3`
-         )}
-         style={{ backgroundColor: backgroundColor, color: textColor }}>
-         {text}
-      </div>
+      <>
+         <div onClick={onClick}
+            className={clsx(
+               `px-2 py-0.5 rounded-md font-semibold`,
+               size === "small" && `text-xs`,
+               size === "medium" && `text-md px-3`,
+               allowDelete && "cursor-pointer hover:opacity-50"
+            )}
+            style={{ backgroundColor: backgroundColor, color: textColor }}>
+            {text}
+         </div>
+      </>
    );
 }
