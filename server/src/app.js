6034f8ef6221be09ebf9c0f7532cf8f019d70da3
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-12 21:17:30 -0300

    testing vercel

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,8 @@ const __dirname = import.meta.dirname;
 
 app.use(cors())
 app.use(express.json())
+app.get('/', (req,res) => res.send('go to /api/v1/tasks'))
+
 //app.use(express.static(path.join(__dirname, "../your-frontend/public")));
 app.use('/api/v1/tasks', tasks)
 
