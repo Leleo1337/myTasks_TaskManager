9d2df60723b995a2e2734c9a9dc5ff7adb0ac465
Leandro <leandroroncatto18@gmail.com>
2025-10-06 15:15:59 -0300

    refactor: remove filter tab functionality for now

--- client/src/components/FiltersTab.tsx
+++ client/src/components/FiltersTab.tsx
@@ -1,11 +1,6 @@
 import type { filtersTabProps } from "../types/filterTypes";
 
-export default function FiltersTab({
-  quantity,
-  filter,
-  onStatusFilterChange,
-  onPriorityFilterChange,
-}: filtersTabProps) {
+export default function FiltersTab({}: filtersTabProps) {
   const btnActive = `bg-blue-300/60 text-blue-700 py-1 px-4 rounded-md border border-blue-500`;
   return (
     <div className="p-4 mb-8 border border-gray-200 rounded-md space-y-4">
@@ -13,36 +8,18 @@ export default function FiltersTab({
         <h4 className="font-semibold text-lg text-gray-500">Status</h4>
         <div className="flex flex-col sm:flex-row gap-4">
           <div className="cursor-pointer">
-            <div
-              onClick={() => onStatusFilterChange("All")}
-              className={
-                filter.status === "All"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              All ({quantity.all})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              All
             </div>
           </div>
           <div className="cursor-pointer">
-            <div
-              onClick={() => onStatusFilterChange("uncompleted")}
-              className={
-                filter.status === "uncompleted"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              uncompleted ({quantity.uncompleted})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              uncompleted
             </div>
           </div>
           <div className="cursor-pointer">
-            <div
-              onClick={() => onStatusFilterChange("Completed")}
-              className={
-                filter.status === "Completed"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              Completed ({quantity.completed})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              Completed
             </div>
           </div>
         </div>
@@ -51,47 +28,23 @@ export default function FiltersTab({
         <h4 className="font-semibold text-lg text-gray-500">Priority</h4>
         <div className="grid grid-cols-2 grid-rows-2 sm:flex sm:flex-row gap-4">
           <div className="cursor-pointer">
-            <div
-              onClick={() => onPriorityFilterChange("All")}
-              className={
-                filter.priority === "All"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              All ({quantity.all})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              All
             </div>
           </div>
           <div className="cursor-pointer">
-            <div
-              onClick={() => onPriorityFilterChange("Low")}
-              className={
-                filter.priority === "Low"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              Low ({quantity.low})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              Low
             </div>
           </div>
           <div className="cursor-pointer">
-            <div
-              onClick={() => onPriorityFilterChange("Medium")}
-              className={
-                filter.priority === "Medium"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              Medium ({quantity.medium})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              Medium
             </div>
           </div>
           <div className="cursor-pointer">
-            <div
-              onClick={() => onPriorityFilterChange("High")}
-              className={
-                filter.priority === "High"
-                  ? btnActive
-                  : `bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`
-              }>
-              High ({quantity.high})
+            <div className={`bg-white py-1 px-4 rounded-md border border-gray-300 hover:bg-gray-100 text-gray-700`}>
+              High
             </div>
           </div>
         </div>
